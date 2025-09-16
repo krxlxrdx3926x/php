1609293361b44f66950eafdf1992329af41b298b
krxlxrdx3926x <144669275+krxlxrdx3926x@users.noreply.github.com>
2025-09-16 18:45:15 +0200

    Add files via upload

---
+++ gruszka.php
@@ -0,0 +1,11 @@
+<?php
+
+echo $_GET["rozwiniecie"] . "<br>";
+echo $_GET["kolor"] . "<br>";
+echo $_GET["miecz"] . "<br>";
+echo $_GET["osiagniecia"] . "<br>";
+echo $_GET["ocena"] . "<br>";
+
+
+
+?> 
